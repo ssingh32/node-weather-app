c97142f60207605eb874ba826ea7d209144d6718
Sukhdip Singh Nijjar <sukhdipsinghnijjar@dhcp-10-15-137-187.wireless.pacific.edu>
2019-07-09 13:08:26 -0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+#Node.js Weather App
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/0c7da7e05aff2cc8910173aa06ef7fb5/' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
